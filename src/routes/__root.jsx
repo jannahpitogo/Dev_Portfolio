891e2e1b4a551064e4c0fc15f1437051c6736bde
jannahpitogo <pitogojannah@outlook.com>
2026-07-26 01:32:13 +0200

    Refactor navigation structure and integrate links in the root component

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -7,7 +7,26 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <h1>My App</h1>
+      <nav className="nav">
+        <header className="nav-header">
+          <div className="logo">
+            <Link to="/">jannahpitogo</Link>
+          </div>
+
+          <div className="nav-links">
+            <Link to="/" hash="#about">
+              About
+            </Link>
+            <Link to="/" hash="#stack">
+              Stack
+            </Link>
+            <Link to="/all-works">Work</Link>
+            <Link to="/creative">Creative</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </header>
+      </nav>
+
       <Outlet />
     </>
   );

5b226a1efe15d49486516b947bf17a9620e9641f
jannahpitogo <pitogojannah@outlook.com>
2026-07-28 13:18:33 +0200

    Update hero and about sections with improved layout and content; enhance styling for better readability

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -39,7 +39,43 @@ function Home() {
 
       <section id="about" className="about">
         <img className="about-img" src={jannahImg} alt="Jannah Pitogo" />
-        <h2 className="about-title">About Me</h2>
+
+        <span className="about-content">
+          <h2 className="about-text">About Me</h2>
+          <p>
+            I’m a digital professional with experience combining technical
+            development and creative design to deliver effective digital
+            solutions. I’ve worked with clients in the Philippines as well as
+            remotely with clients in Singapore and the United States, helping
+            create responsive websites and strong visual branding.
+          </p>
+          <p>
+            My expertise includes: Web Development: Developing responsive sites
+            using HTML, CSS, and JavaScript, with a focus on deployment and site
+            security (Cloudflare).
+          </p>
+          <p>
+            Creative Design: Professional video editing, 3D modeling, and brand
+            asset creation using Adobe Creative Suite (Premiere Pro,
+            Illustrator, Photoshop) and Blender.
+          </p>
+
+          <p>
+            Digital Strategy: Streamlining business operations through marketing
+            automation, social media management, and SEO-optimized profiles.
+          </p>
+          <p>
+            Administrative: Managing complex data workflows, CRM updates, and
+            real estate project coordination to ensure seamless business
+            delivery. I enjoy learning how technology works and helping others
+            understand it more clearly. I’m naturally curious and enjoy
+            exploring problems that I don't understand. One of the things I
+            value most when working with others is clear communication and
+            collaboration. I also value the freedom in communication where
+            everyone can share their ideas without barriers. Let’s connect:
+            pitogojannah@outlook.com
+          </p>
+        </span>
       </section>
     </main>
   );

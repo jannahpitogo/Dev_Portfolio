059b3adc82fb54777d7823731cc12c809af46851
jannahpitogo <pitogojannah@outlook.com>
2026-08-06 13:57:14 +0200

    refactor: create Work component to encapsulate project display logic

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Link } from "@tanstack/react-router";
-import projects from "../data/projects.json";
 import AnnouncementModal from "../components/modal.jsx";
 import About from "../components/about.jsx";
 import Hero from "../components/hero.jsx";
 import TechStack from "../components/techstack.jsx";
+import Work from "../components/work.jsx";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -17,60 +16,7 @@ function Home() {
       <Hero />
       <About />
       <TechStack />
-
-      <section id="work" className="work">
-        <div className="work-header">
-          <h2 className="work-text">Selected Works</h2>
-          <Link to="/projects" className="work-main_button">
-            Playground {">"}
-          </Link>
-        </div>
-        <div className="work-projects">
-          {projects.map((project, index) => (
-            <div
-              className="project"
-              key={`${project.project_title}-${project.project_date}`}
-            >
-              <a href={project.demo} target="_blank" rel="noopener noreferrer">
-                <img
-                  src={
-                    index === 0 ? "images/premieredent.jpg" : project.background
-                  }
-                  alt={project.title}
-                  className="project-img"
-                />
-              </a>
-
-              <div className="project-content">
-                <h3 className="work-project-title">{project.project_title}</h3>
-                <p className="work-project-description">
-                  {project.description}
-                </p>
-
-                <div className="project-button-container">
-                  <a
-                    href={project.demo}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="work-project-button"
-                  >
-                    View Project
-                  </a>
-
-                  <a
-                    href={project.code}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="work-project-button"
-                  >
-                    Github Repository
-                  </a>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
+      <Work />
 
       <section id="contact" className="contact">
         <div className="contact-header">

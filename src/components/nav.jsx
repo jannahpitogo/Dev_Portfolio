15ccf3aefc1d9af8d0f965de661630716d6e2eff
jannahpitogo <pitogojannah@outlook.com>
2026-08-16 07:40:53 +0200

    feat: add Nav component for navigation links and social media integration in __root.jsx

---
+++ src/components/nav.jsx
@@ -0,0 +1,56 @@
+import { Link } from "@tanstack/react-router"
+
+export default function Nav(){
+    return (
+
+        <nav className="nav">
+                <header className="nav-header">
+                <div className="logo">
+                    <Link to="/">jannahpitogo</Link>
+                </div>
+
+                <div className="nav-links">
+                    <Link to="/" hash="about">
+                    ABOUT
+                    </Link>
+                    <Link to="/" hash="stack">
+                    STACK
+                    </Link>
+                    <Link to="/" hash="work">
+                    WORK
+                    </Link>
+                    <Link to="/" hash="contact">
+                    CONTACT
+                    </Link>
+
+                    <div className="nav-socials">
+                    <a
+                        href="https://github.com/jannahpitogo"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                        src="images/github.png"
+                        alt="github logo"
+                        className="link-logo"
+                        />
+                    </a>
+
+                    <a
+                        href="https://www.linkedin.com/in/jannah-pitogo/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                        src="images/linkedin.webp"
+                        alt="linkedin logo"
+                        className="link-logo"
+                        />
+                    </a>
+                    </div>
+                </div>
+                </header>
+        </nav>
+    );     
+};
+        
--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -2,6 +2,7 @@ import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import "../index.css";
 import StackIcon from "tech-stack-icons";
 import Footer from "../components/footer.jsx";
+import Nav from "../components/nav.jsx";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -10,56 +11,9 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <nav className="nav">
-        <header className="nav-header">
-          <div className="logo">
-            <Link to="/">jannahpitogo</Link>
-          </div>
-
-          <div className="nav-links">
-            <Link to="/" hash="about">
-              ABOUT
-            </Link>
-            <Link to="/" hash="stack">
-              STACK
-            </Link>
-            <Link to="/" hash="work">
-              WORK
-            </Link>
-            <Link to="/" hash="contact">
-              CONTACT
-            </Link>
-
-            <div className="nav-socials">
-              <a
-                href="https://github.com/jannahpitogo"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="images/github.png"
-                  alt="github logo"
-                  className="link-logo"
-                />
-              </a>
-
-              <a
-                href="https://www.linkedin.com/in/jannah-pitogo/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="images/linkedin.webp"
-                  alt="linkedin logo"
-                  className="link-logo"
-                />
-              </a>
-            </div>
-          </div>
-        </header>
-      </nav>
-
       
+
+      <Nav/>
 
       <Outlet />
       <Footer/>

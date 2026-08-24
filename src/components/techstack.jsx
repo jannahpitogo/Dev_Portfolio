21380a28cc8f4c42e3ba8bc7f8f99dc1a8d29d6e
jannahpitogo <pitogojannah@outlook.com>
2026-08-24 12:51:44 +0200

    feat: add tech stack icons and update TechStack component for improved visual representation

--- src/components/techstack.jsx
+++ src/components/techstack.jsx
@@ -1,61 +1,57 @@
-import StackIcon from "tech-stack-icons";
-
 export default function TechStack() {
   return (
     <section id="stack" className="stack">
       <h2 className="stack-text">Tech Stack</h2>
 
       <div className="stack-icons">
-        <span>
-          <StackIcon name="html5" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/html.png" alt="HTML" className="stack-icon" width="50" height="50"/>
           <p>HTML5</p>
         </span>
-        <span>
-          <StackIcon name="css3" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/css.png" alt="CSS" className="stack-icon" width="50" height="50"/>
           <p>CSS3</p>
         </span>
-        <span>
-          <StackIcon name="js" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/js.webp" alt="javaScript" className="stack-icon" width="50" height="50"/>
           <p>JavaScript</p>
         </span>
-        <span>
-          <StackIcon name="tailwindcss" className="stack-icon" />
-          <p>Tailwind CSS</p>
-        </span>
-        <span>
-          <StackIcon name="react" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/react.webp" alt="React" className="stack-icon mb-1.5" width="50" height="50"/>
           <p>React</p>
         </span>
-        <span>
-          <StackIcon name="nodejs" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/nodejs.webp" alt="NodeJS" className="stack-icon" width="50" height="50"/>
           <p>Node.js</p>
         </span>
-        <span>
-          <img
-            src="images/express.webp"
-            alt="Express.js"
-            className="stack-icon"
-          />
-          <p>Express.js</p>
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/tailwindcss.png" alt="TailwindCSS" className="stack-icon" width="70" height="70"/>
+          <p>Tailwind</p>
         </span>
-        <span>
-          <StackIcon name="mongodb" className="stack-icon" />
-          <p>MongoDB</p>
-        </span>
-        <span>
-          <StackIcon name="git" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/git.png" alt="Git" className="stack-icon" width="50" height="50"/>
           <p>Git</p>
         </span>
-        <span>
-          <img src="images/github.png" alt="GitHub" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img
+            src="./src/assets/icons/express.webp"
+            alt="Express.js"
+            className="stack-icon"
+            width="50"
+            height="50"
+          />
+          <p>Express</p>
+        </span>
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/github.png" alt="GitHub" className="stack-icon" width="50" height="50"/>
           <p>GitHub</p>
         </span>
-        <span>
-          <StackIcon name="mysql" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/mysql.png" alt="MySQL" className="stack-icon" width="50" height="50"/>
           <p>MySQL</p>
         </span>
-        <span>
-          <StackIcon name="python" className="stack-icon" />
+        <span className="tech-icon-container">
+          <img src="./src/assets/icons/python.webp" alt="Python" className="stack-icon" width="50" height="50"/>
           <p>Python</p>
         </span>
       </div>

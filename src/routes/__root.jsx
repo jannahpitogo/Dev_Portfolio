a607e8d5243cc75bcbc2392f24ac453f535492ef
jannahpitogo <pitogojannah@outlook.com>
2026-08-24 13:55:19 +0200

    chore: remove unused StackIcon import from RootComponent

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,6 +1,5 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import "../index.css";
-import StackIcon from "tech-stack-icons";
 import Footer from "../components/footer.jsx";
 import Nav from "../components/nav.jsx";
 
